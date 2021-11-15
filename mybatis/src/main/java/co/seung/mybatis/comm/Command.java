d1b4a808f9c4270b20fabb1f53b328901b0ff43f
admin <admin@YD01-22>
2021-11-15 09:38:28 +0900

    git test

--- mybatis/src/main/java/co/seung/mybatis/comm/Command.java
+++
@@ -1,8 +0,0 @@
-package co.seung.mybatis.comm;
-
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
-public interface Command {
-	public String run(HttpServletRequest request, HttpServletResponse response);
-}
